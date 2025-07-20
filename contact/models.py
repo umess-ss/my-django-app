87493c37bc02452c00a7c3813a0df38699c788cf
Umesh Rajbanshi <umesh@example.com>
2025-07-20 14:40:38 +0545

    Updated Feedback Form

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from post.models import Post
 
 # Create your models here.
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    message = models.TextField(max_length=1000)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,null=True, blank=True)
+
+    class Meta:
+        db_table = 'feedbacks'
--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Feedback
+
 
 # Create your views here.
 def contact(request):
+    if (request.method == 'POST'):
+        name = request.POST.get('name')
+        email = request.POST['email']
+        message = request.POST['message']
+        feedback = Feedback(
+            name = name,
+            email = email,
+            message = message
+        )
+        feedback.save()
+        return redirect("index")
     return render(request,'contact/contact.html')
